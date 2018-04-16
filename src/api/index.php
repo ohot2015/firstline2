886252e92f0374532452127d190d5b72a9f24091
Unknown <ohot2015@gmail.com>
2018-04-16 22:54:11 +0300

    fix

--- src/api/index.php
+++ src/api/index.php
@@ -4,8 +4,7 @@ require_once 'class/_autoload.php';
 
 // $detect = new Mobile_Detect;
 
-echo $_SERVER['DOCUMENT_ROOT'] . 'firstline2/gallery';
-exit;
-$gallery  = new StreamGallery($_SERVER['DOCUMENT_ROOT'] . 'firstline2/gallery', '/gallery');
+
+$gallery  = new StreamGallery($_SERVER['DOCUMENT_ROOT'] . '/gallery', '/gallery');
 echo json_encode(['images' => $gallery->getData(),'stream' =>$gallery->getStream()]);
 ?>
